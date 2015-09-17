2191c1a95dd7bdf2840a59c12d3dcd250b2bed3f
jayweb <jayrdee@gmail.com>
2015-09-17 15:24:01 +0200

    first commit

---
+++ Classes/objects.php
@@ -0,0 +1,6 @@
+<?php 
+# Objects.php
+
+
+
+?>
---
+++ index_template.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="eng">
+
+<head>
+<meta charset="utf-8" />
+<title>Home</title>
+<link href= rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+</body>
+</html>
