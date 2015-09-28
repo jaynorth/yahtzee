8db17069208544f1b089c00311d0eddea0395209
jayweb <jayrdee@gmail.com>
2015-09-29 00:45:03 +0200

    added some css

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
 </head>
 
-<body > <!-- onload="initElement();" -->
+<body >
 <h1>Yahtzee Project</h1>
 <form>
 	<input type="button" id="rollButton" value="Roll">
@@ -31,22 +31,22 @@
 		<td>UPPER SECTION</td><td>HOW TO SCORE</td><td>score</td>		
 	</tr>
 	<tr>
-		<td>ACE</td><td>COUNT and add</td><td id="ace" onmouseover="showMouseover('ace', 1)" onmouseout="showMouseOut('ace')" onclick="addToScore('ace', 1)"> </td>	
+		<td>ACE</td><td>COUNT and add</td><td class="scoredata" id="ace" onmouseover="showMouseover('ace', 1)" onmouseout="showMouseOut('ace')" onclick="addToScore('ace', 1)"> </td>	
 	</tr>
 	<tr>
-		<td>TWOs</td><td>COUNT and add</td><td id="twos" onmouseover="showMouseover('twos', 2)" onmouseout="showMouseOut('twos')" onclick="addToScore('twos', 2)"></td>		
+		<td>TWOs</td><td>COUNT and add</td><td class="scoredata" id="twos" onmouseover="showMouseover('twos', 2)" onmouseout="showMouseOut('twos')" onclick="addToScore('twos', 2)"></td>		
 	</tr>
 	<tr>
-		<td>Threes</td><td>COUNT and add</td><td id="threes" onmouseover="showMouseover('threes', 3)" onmouseout="showMouseOut('threes')" onclick="addToScore('threes', 3)"></td>	
+		<td>Threes</td><td>COUNT and add</td><td class="scoredata" id="threes" onmouseover="showMouseover('threes', 3)" onmouseout="showMouseOut('threes')" onclick="addToScore('threes', 3)"></td>	
 	</tr>
 	<tr>
-		<td>Fours</td><td>COUNT and add</td><td id="fours" onmouseover="showMouseover('fours', 4)" onmouseout="showMouseOut('fours')" onclick="addToScore('fours', 4)"></td>		
+		<td>Fours</td><td>COUNT and add</td><td class="scoredata" id="fours" onmouseover="showMouseover('fours', 4)" onmouseout="showMouseOut('fours')" onclick="addToScore('fours', 4)"></td>		
 	</tr>
 	<tr>
-		<td>Fives</td><td>COUNT and add</td><td id="fives" onmouseover="showMouseover('fives', 5)" onmouseout="showMouseOut('fives')" onclick="addToScore('fives', 5)"></td>	
+		<td>Fives</td><td>COUNT and add</td><td class="scoredata" id="fives" onmouseover="showMouseover('fives', 5)" onmouseout="showMouseOut('fives')" onclick="addToScore('fives', 5)"></td>	
 	</tr>
 	<tr>
-		<td>Sixes</td><td>COUNT and add</td><td id="sixes" onmouseover="showMouseover('sixes', 6)" onmouseout="showMouseOut('sixes')" onclick="addToScore('sixes', 6)"></td>	
+		<td>Sixes</td><td>COUNT and add</td><td class="scoredata" id="sixes" onmouseover="showMouseover('sixes', 6)" onmouseout="showMouseOut('sixes')" onclick="addToScore('sixes', 6)"></td>	
 	</tr>
 	<tr>
 		<td>BONUS</td><td>if total is 63 or over</td><td></td>		
