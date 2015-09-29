01a45efcc25e86db3c9eb19b337564806e78e9fd
jayweb <jayrdee@gmail.com>
2015-09-29 21:46:34 +0200

    now small straight and big straight are logged, messy code and needs to be tested a bit more

--- js/scoreTable.js
+++ js/scoreTable.js
@@ -100,18 +100,22 @@ function checkDiceHand(object){
     var keyList=[];
 
     for (var key in object){
-        keyList.push([key]);
+        
+
+        keyList.push(parseInt(key));//parsInt converts string to int
+
+
         var value = object[key];
         console.log(key + ': ' +value);
 
         if (value==5){
             console.log('yahtzee');
-            Yahtzee+=1;
+            Yahtzee=1;
             break;
         }
         if (value==4){
             console.log('four of a kind');
-            fourOfaKind+=1;
+            fourOfaKind=1;
             break;
         }
         if (value==3){
@@ -127,36 +131,50 @@ function checkDiceHand(object){
             //console.log('Possible straight');
             fullHouse -=1;
             straightCheck +=1;
-        }
-
-       
+        }  
         
 
 
     }
 
-    console.log('keylist is :' + keyList);
-    console.log('straightcheck is ' +straightCheck);
+    console.log('keylist is');
+    console.log(keyList);
+
+    console.log('straightcheck is ' + straightCheck);
 
     if (fullHouse==2){
         console.log('full House');
     }
-  
-        console.log('straight check small straight possible');
-        arr1= [2, 3, 4, 5];
-        arr2= [1, 2, 3, 4];
-        arr3= [3, 4, 5, 6];
-        console.log(arr1);
-        if(keyList===arr1 || keyList===arr2 || keyList===arr3){
-            console.log('SMALL STRAIGHT!!!!!!!!!!!! : ' + keyList);
+
+    if (straightCheck==5){
+        console.log('straightcheck = 5');
+        console.log(keyList[0]);
+        console.log(keyList[4]);
+        var a = keyList[0];
+        console.log('following is var a ');
+        console.log(a+4);
+        var b = keyList[4];
+        if (b == a+4){
+            console.log('It is a Big Straight !!!!');
+        }else if (keyList.toString()==[1, 2, 3, 4, 6 ].toString() || keyList.toString()===[1, 3, 4, 5, 6].toString() ){
+            console.log('We got a small straight, no pair!!!');
+        }else{
+            console.log('We got no small straight with no pair!');
         }
 
-     
-        
-    
-    if (straightCheck==5 ){
-        console.log('BIG STRAIGHT : ' + keyList);
     }
+
+    if (straightCheck==3){
+        console.log('checking for small straight with a pair');
+        if (keyList.toString()==[1, 2, 3, 4].toString() || keyList.toString()===[2, 3, 4, 5].toString() || keyList.toString()===[3, 4, 5, 6].toString() ){
+            console.log('We got a small straight with a pair!!!');
+        }else{
+            console.log('We got no small straight with a pair!');
+        }
+
+    }
+
+    
 
 
 };
